25be0502783c9c2e0635a56478481f3f54030f34
sumon-ray <raysumon220020@gmail.com>
2025-04-30 23:19:52 +0600

    auth middleware updated

--- src/app/middlewares/Auth.ts
+++ src/app/middlewares/Auth.ts
@@ -14,7 +14,8 @@ const auth = (...roles: string[]) => {
     next: NextFunction,
   ) => {
     try {
-  const token = req.headers.authorization?.split(' ')[1]; 
+      const token = req.headers.authorization;
+  // const token = req.headers.authorization?.split(' ')[1]; 
 
       //   console.log(token);
       if (!token) {
