6bc2ff7b225618f109d8a2134fac2ee26b10341d
sumon-ray <raysumon220020@gmail.com>
2025-04-30 08:14:36 +0600

    added user login functionality

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
-import catchAsync from '../../../../../../SQL/Prisma/PH_health_care/src/shared/catchAsync';
-import sendResponse from '../../../../../../SQL/Prisma/PH_health_care/src/shared/sendResponse';
+import catchAsync from '../../helpers/catchAsync';
+import sendResponse from '../../helpers/sendResponse';
 import { authService } from './auth.service';
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
@@ -18,7 +18,6 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
     message: 'Logged in user successfully',
     data: {
       accessToken: result.accessToken,
-      needPasswordChange: result.needPasswordChange,
     },
   });
 });
@@ -34,50 +33,8 @@ const refreshToken = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
-//
-const passwordChange = catchAsync(
-  async (req: Request & { user?: any }, res: Response) => {
-    const user = req.user;
-    const result = await authService.passwordChange(user, req.body);
 
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: 'password changed successfully',
-      data: result,
-    });
-  },
-);
-
-const forgotPassword = catchAsync(async (req: Request, res: Response) => {
-  await authService.forgotPassword(req.body);
-
-  sendResponse(res, {
-    success: true,
-    statusCode: httpStatus.OK,
-    message: 'check your email',
-    data: null,
-  });
-});
-
-// reset-password
-const resetPassword = catchAsync(async (req: Request, res: Response) => {
-  //   const token = req.headers.authorization || "";
-  const token = req.headers.authorization?.replace('Bearer ', '') || '';
-
-  await authService.resetPassword(token, req.body);
-
-  sendResponse(res, {
-    success: true,
-    statusCode: httpStatus.OK,
-    message: 'password reset ',
-    data: null,
-  });
-});
 export const authControlller = {
   loginUser,
   refreshToken,
-  passwordChange,
-  forgotPassword,
-  resetPassword,
 };
