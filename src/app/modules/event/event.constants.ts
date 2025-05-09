58b3e54293bfe593a411c052d0a00ef484446075
Sajadur Rahman <itzmesojib@gmail.com>
2025-05-09 14:35:26 +0600

    Delete src/app/modules directory

--- src/app/modules/event/event.constants.ts
+++
@@ -1,3 +0,0 @@
-export const eventSearchableFields = ['title', 'description', 'venue'];
-
-export const eventFilterableableFields = ['title', 'description', 'venue', 'searchTerm', 'minFee', 'maxFee', 'isPaid','isPrivate','eventStatus' , 'category' , 'startDate' , 'endDate']; 
