645bb102edd284f03b1ff29815c826e64d8caab1
Sajadur Rahman <itzmesojib@gmail.com>
2025-05-12 18:23:20 +0600

    Delete src directory

--- src/app/modules/payment/payment.utils.ts
+++
@@ -1,6 +0,0 @@
-
-export const generateTransactionId = (): string => {
-    const timestamp = Date.now().toString().slice(-6);
-    const randomString = Math.random().toString(36).substring(2, 8).toUpperCase();
-    return `${"NEXT"}${timestamp}${randomString}`;
-};
