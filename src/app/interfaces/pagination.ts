ea4d780a32bb3448416536cb221fe890cd381814
Sajadur Rahman <itzmesojib@gmail.com>
2025-05-12 18:30:37 +0600

    Delete src directory

--- src/app/interfaces/pagination.ts
+++
@@ -1,6 +0,0 @@
-export type IPaginationOptions = {
-    page?: number,
-    limit?: number,
-    sortOrder?: string,
-    sortBy?: string
-}
--- src/app/modules/profile/profile.constraint.ts
+++
@@ -1,9 +0,0 @@
-export const selectFields = {
-    id: true,
-    name: true,
-    email: true,
-    address: true,
-    phoneNumber: true,
-    profileImage: true,
-    occupation: true,
-  };
